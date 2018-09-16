14620b81e10b0fba8a88d023d5cbbce21220f367
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-09-16 10:34:04 +0530

    changed back to shutdown trigger

--- shutdown-press-simple.py
+++ shutdown-press-simple.py
@@ -4,7 +4,6 @@ try:
     from gpiozero import Button
 
     Button(21).wait_for_press()
-    os.system("python neural_network_infinate.py\n")
-    # os.system("sudo poweroff")
+    os.system("sudo poweroff")
 except Exception as e:
     open('console_log.txt', 'a').write(str(e))
