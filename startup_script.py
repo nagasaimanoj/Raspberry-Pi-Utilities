ad41526c8a6209e52af6a83eb3bb45c8aba606dd
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-10-04 00:07:53 +0530

    comments added

--- startup_script.py
+++ startup_script.py
@@ -5,21 +5,25 @@ from gpiozero import Button
 
 
 def restart():
+    # restart on sorting GPIO 26 with GND
     Button(26).wait_for_press()
     system("sudo init 6 \n")
 
 
 def shutdown():
+    # shutdown on sorting GPIO 21 with GND
     Button(21).wait_for_press()
     system("sudo init 0 \n")
 
 
 def update_repo():
+    # updates the current repo with remote repo
     system('cd /hpme/pi/GNSMK/Raspberry-Pi-Utilities/ \n')
     system('git pull origin master \n')
 
 
 def update_linux():
+    # updates / upgrades linux
     system('sudo apt-get update \n')
     system('sudo apt-get upgrade -y \n')
     system('sudo apt-get dist-upgrade -y \n')
