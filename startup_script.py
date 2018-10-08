a3b104cf82fc228ed4f937c2fbd0f1b330cef933
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-10-08 12:19:09 +0530

    removed explicit \n from each line

--- startup_script.py
+++ startup_script.py
@@ -7,19 +7,19 @@ from gpiozero import Button
 def restart():
     # restart on sorting GPIO 26 with GND
     Button(26).wait_for_press()
-    system("sudo init 6 \n")
+    system('sudo init 6')
 
 
 def shutdown():
     # shutdown on sorting GPIO 21 with GND
     Button(21).wait_for_press()
-    system("sudo init 0 \n")
+    system('sudo init 0')
 
 
 def update_repo():
     # updates the current repo with remote repo
-    system('cd /hpme/pi/GNSMK/Raspberry-Pi-Utilities/ \n')
-    system('git pull origin master \n')
+    system('cd /hpme/pi/GNSMK/Raspberry-Pi-Utilities')
+    system('git pull origin master')
 
 
 Process(target=restart).start()
