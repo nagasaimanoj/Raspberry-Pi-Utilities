50b45bb7ec156bcb87afcd73633a0173162211b5
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-10-10 12:20:43 +0530

    comments changed

--- startup_script.py
+++ startup_script.py
@@ -4,20 +4,20 @@ from os import system
 from gpiozero import Button
 
 
-def restart():
-    # restart on sorting GPIO 26 with GND
-    Button(26).wait_for_press()
-    system('sudo init 6')
-
-
 def shutdown():
-    # shutdown on sorting GPIO 21 with GND
+    # GPIO_21 + GND = shutdown
     Button(21).wait_for_press()
     system('sudo init 0')
 
 
+def restart():
+    # GPIO_26 + GND  = restart
+    Button(26).wait_for_press()
+    system('sudo init 6')
+
+
 def update_dir():
-    # updates the current dir with remote repo
+    # updates Utilities scripts
     system('cd /hpme/pi/GNSMK/Raspberry-Pi-Utilities')
     system('git pull origin master')
 
