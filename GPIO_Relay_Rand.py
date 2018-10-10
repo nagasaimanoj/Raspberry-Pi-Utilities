809e17631dbff431a35ec28dd50b78518fee1718
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-10-10 12:15:14 +0530

    everything changed to random

--- GPIO_Relay_Rand.py
+++ GPIO_Relay_Rand.py
@@ -1,5 +1,5 @@
+from random import randint, random
 from time import sleep
-from random import random
 
 from RPi import GPIO
 
@@ -9,6 +9,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(pins, GPIO.OUT)
 
 while True:
-    for each_pin in pins:
-        GPIO.output(each_pin,  int(random()))
-        sleep(0.5)
+    GPIO.output(
+        pins[randint(0, 3)],
+        randint(0, 1)
+    )
+    sleep(random())
