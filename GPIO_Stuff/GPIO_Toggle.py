b202ce11aba8f7ead92dad5ae51ea2b5ca2e496b
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-11-06 00:18:46 +0530

    this can toggle a given GPIO

---
+++ GPIO_Stuff/GPIO_Toggle.py
@@ -0,0 +1,16 @@
+from sys import argv
+from time import sleep
+
+from RPi import GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO_pin = int(argv[1])
+GPIO_status = int(argv[2])
+
+GPIO.setup(GPIO_pin, GPIO.OUT)
+
+GPIO.output(
+    GPIO_pin, GPIO_status
+)
