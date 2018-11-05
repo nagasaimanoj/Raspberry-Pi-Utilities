8aec5c2aa7a69271723b08c3b472f4b8607f2a0e
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-11-06 00:18:58 +0530

    this can return GPIO pin state

---
+++ GPIO_Stuff/GPIO_status.py
@@ -0,0 +1,16 @@
+from sys import argv
+from time import sleep
+
+from RPi import GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO_pin = int(argv[1])
+GPIO.setup(GPIO_pin, GPIO.OUT)
+
+current_state = not GPIO.input(
+    GPIO_pin
+)
+
+print(int(current_state))
