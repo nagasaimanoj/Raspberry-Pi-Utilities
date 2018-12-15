9940d72bac1483a52c219c64249b1bb4caa2feb5
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-12-15 18:00:36 +0000

    log-file name changed

--- startup_script.py
+++ startup_script.py
@@ -6,7 +6,7 @@ import time
 import gpiozero
 
 logging.basicConfig(
-    filename='natalie',
+    filename='natalie.log',
     level=logging.DEBUG,
     format='%(asctime)s %(levelname)-8s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
