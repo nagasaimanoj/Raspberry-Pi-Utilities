f29a944964cd2c5638c42822bf7f959bcbb4a5e2
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-12-25 02:01:02 +0530

    package imports optimised

--- startup_script.py
+++ startup_script.py
@@ -1,13 +1,13 @@
-import logging
-import multiprocessing
-import os
-import time
+from logging import DEBUG, basicConfig, debug
+from multiprocessing import Process
+from os import chdir, popen, system
+from time import sleep
 
-import gpiozero
+from gpiozero import Button
 
-logging.basicConfig(
+basicConfig(
     filename='/home/pi/GNSMK/natalie.log',
-    level=logging.DEBUG,
+    level=DEBUG,
     format='%(asctime)s %(levelname)-8s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
@@ -15,25 +15,25 @@ logging.basicConfig(
 
 def log(message):
     print(message)
-    logging.debug(message)
+    debug(message)
 
 
 def shutdown():
     # GPIO_21 + GND = shutdown
 
     log('waiting for shutdown trigger')
-    gpiozero.Button(21).wait_for_press()
+    Button(21).wait_for_press()
 
-    os.system('sudo init 0')
+    system('sudo init 0')
 
 
 def restart():
     # GPIO_26 + GND  = restart
 
     log('waiting for restart trigger')
-    gpiozero.Button(26).wait_for_press()
+    Button(26).wait_for_press()
 
-    os.system('sudo init 6')
+    system('sudo init 6')
 
 
 def update_dir():
@@ -44,9 +44,9 @@ def update_dir():
     log('updating utilities dir')
 
     log('changing git dir')
-    path_change_err = os.chdir('git_dir_path')
+    path_change_err = chdir('git_dir_path')
 
-    if os.system('pwd') == git_dir_path:
+    if system('pwd') == git_dir_path:
         log('path change success')
 
         git_result = os.system('git pull origin master')
@@ -60,10 +60,10 @@ def show_temp():
     # logs processor temperature for every second
 
     while True:
-        temp = os.popen("vcgencmd measure_temp").readline()
+        temp = popen("vcgencmd measure_temp").readline()
         log('cpu_temp' + str(temp))
 
-        time.sleep(1)
+        sleep(1)
 
 
 func_list = [
@@ -76,6 +76,6 @@ func_list = [
 for each_func in func_list:
     log('starting ' + each_func.__name__)
 
-    multiprocessing.Process(target=each_func).start()
+    Process(target=each_func).start()
 
     log(each_func.__name__ + 'started')
